7f5a299113e2ad3b56eb04689b745125accfb29c
wilg <wilsonreddy@hotmail.com>
2016-04-22 12:43:03 -0700

    made localhost https..

--- tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Web/wwwroot/app/app.js
+++ tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Web/wwwroot/app/app.js
@@ -4,67 +4,64 @@ angular.module('matterMain', [
   'ngResource',
   'ui.router',
   'AdalAngular',
-  'ngTouch',
   'ui.grid'
 ])
 .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', 'adalAuthenticationServiceProvider',
     function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider, adalProvider) {
 
-          // For any unmatched url, send to /route1
-            $urlRouterProvider.otherwise(function($injector, $location) {
-                var $state = $injector.get("$state");
-                $state.go("mc.navigation");
-            });
+        // For any unmatched url, send to /route1
+        $urlRouterProvider.otherwise(function ($injector, $location) {
+            var $state = $injector.get("$state");
+            $state.go("mc.navigation");
+        });
 
-			$stateProvider
-           .state('mc', {
-               url: '/',
-               views: {
-                   "view": {
-                       templateUrl: '/app/home.html',
-                       controller: 'homeController as vm'
-                   }
-               }, requireADLogin: true
-           })
-		     .state('mc.navigation', {
-		      url: "^/navigation",
-		      views: {
-		          "contentView": {
-		              templateUrl: '/app/navigation.html',
-		              controller: 'navigationController as vm'
-		          }
-		      }, requireADLogin: true
-		    })
-			.state('mc.matters', {
-		      url: "^/matters",
-		      views: {
-		          "contentView": {
-		              templateUrl: '/app/matter/matters.html',
-		              controller: 'mattersController as vm'
-		          }
-		      }, requireADLogin: true
-		    });
+        $stateProvider
+       .state('mc', {
+           url: '/',
+           views: {
+               "view": {
+                   templateUrl: '/app/home.html',
+                   controller: 'homeController as vm'
+               }
+           }, requireADLogin: true
+       })
+         .state('mc.navigation', {
+             url: "^/navigation",
+             views: {
+                 "contentView": {
+                     templateUrl: '/app/navigation.html',
+                     controller: 'navigationController as vm'
+                 }
+             }, requireADLogin: true
+         })
+        .state('mc.matters', {
+            url: "^/matters",
+            views: {
+                "contentView": {
+                    templateUrl: '/app/matter/matters.html',
+                    controller: 'mattersController as vm'
+                }
+            }, requireADLogin: true
+        });
 
-			            $locationProvider.html5Mode({
-                enabled: false,
-                requireBase: false
-            });
-        
-      var endpoints = {
-          
-      };
-//'https://mattercenterservicenew.azurewebsites.net
-        //http://localhost:58775
-    adalProvider.init(
-     {
-         instance: 'https://login.microsoftonline.com/',
-		 tenant: '72f988bf-86f1-41af-91ab-2d7cd011db47',
-         clientId: '83b15df9-0dca-4137-a47f-cd69cffc3df8',
-         extraQueryParameter: 'nux=1',
-         cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.
-         endpoints: endpoints
-     }, $httpProvider);
+        $locationProvider.html5Mode({
+            enabled: false,
+            requireBase: false
+        });
 
+        var endpoints = {
+            "https://localhost:44323": "83b15df9-0dca-4137-a47f-cd69cffc3df8"
+        };
+        adalProvider.init(
+         {
+             instance: 'https://login.microsoftonline.com/',
+             tenant: 'microsoft.onmicrosoft.com',
+             clientId: '83b15df9-0dca-4137-a47f-cd69cffc3df8',
+             extraQueryParameter: 'nux=1',
+             cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.
+             endpoints: endpoints
+         }, $httpProvider);
+         
 
-  }])
+    }]);
 
