e2d91d81f1ee2a5b2a05279c64266af319878d70
wilg <wilsonreddy@hotmail.com>
2016-04-27 16:35:26 -0700

    added mattercenter css and made home page look and feel similar to current matter center.

--- tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Web/wwwroot/app/navigation.controller.js
+++ tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Web/wwwroot/app/navigation.controller.js
@@ -5,7 +5,23 @@
         .controller('navigationController', ['$scope', '$state', '$stateParams', 'api',
         function ($scope, $state, $stateParams, api) {
 
-    
+            var vm = this;
+
+            vm.menuClick = function () {
+                var oAppMenuFlyout = $(".AppMenuFlyout");
+                if (!(oAppMenuFlyout.is(":visible"))) {
+                    //// Display the close icon and close the fly out
+                    $(".OpenSwitcher").addClass("hide");
+                    $(".CloseSwitcher").removeClass("hide");
+                    $(".MenuCaption").addClass("hideMenuCaption");
+                    oAppMenuFlyout.slideDown();
+                } else {
+                    oAppMenuFlyout.slideUp();
+                    $(".CloseSwitcher").addClass("hide");
+                    $(".OpenSwitcher").removeClass("hide");
+                    $(".MenuCaption").removeClass("hideMenuCaption");
+                }
+            }
           
 
         }]);
