0a9b52533e7960d97f654f8227a2752cd9232d18
venkatesh127 <v-venmup@microsoft.com>
2016-05-02 06:42:10 -0700

    Written Angular code for createMatter functionality

--- tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Web/wwwroot/app/matter/createMatter.controller.js
+++ tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Web/wwwroot/app/matter/createMatter.controller.js
@@ -12,7 +12,15 @@
             cm.popupContainerBackground = "Show";
             cm.popupContainer = "hide";
             cm.areaOfLawTerms = [];
-            cm.subAreaOfLawTerms = [];
+            cm.subAreaOfLawTerms = [];        
+            cm.documentTypeLawTerms = [];
+            cm.activeAOLTerm = null;
+            cm.activeSubAOLTerm = null;
+            cm.activeDocumentTypeLawTerm = null;
+            cm.sectionName = "snOpenMatter";
+            cm.removeDTItem = false;
+            
+           
             var optionsForClientGroup = new Object;
             var optionsForPracticeGroup = new Object;
 
@@ -70,29 +78,37 @@
 
             });
 
+            //calls this function when selectType button clicks
             cm.selectMatterType = function () {
                 cm.popupContainerBackground = "Show";
-                cm.popupContainer = "Show";
-                getTaxonomyDetailsForPractice(optionsForPracticeGroup, function (response) {
-                    cm.pracitceGroupList = response.pgTerms;
-                });
+                if (cm.pracitceGroupList == null) {
+                    getTaxonomyDetailsForPractice(optionsForPracticeGroup, function (response) {
+                        cm.pracitceGroupList = response.pgTerms;
+                        cm.popupContainer = "Show";
+                        cm.popupContainerBackground = "Show";
+                    });
+                }
+                else {
+                    cm.popupContainer = "Show";
+                    cm.popupContainerBackground = "Show";
+                }
             }
 
-
-            cm.selectMatterTypePopUpClose = function () {
-                alert();
+            //function for closing the popup
+            cm.selectMatterTypePopUpClose = function () {               
                 cm.popupContainerBackground = "hide";
                 cm.popupContainer = "hide";
             }
-
+            //function to get the clientId from ClientName dropdown
             cm.getSelectedClientValue = function (client) {
-
+                
                 cm.clientId = cm.selectedClientName;
 
             }
+            //function to get the all AOL and SAOL terms
             cm.getSelectedPracticeGroupValue = function () {
 
-
+               
                 if (cm.selectedPracticeGroup != null) {
                     cm.areaOfLawTerms = cm.selectedPracticeGroup.areaTerms;
                     cm.subAreaOfLawTerms = cm.selectedPracticeGroup.areaTerms[0].subareaTerms;
@@ -105,8 +121,8 @@
 
             }
 
-
-            cm.selectedAreaOfLawTerm = function (areaOfLawTerm) {
+            //function to get the subAOL items on selection of AOLTerm
+            cm.selectAreaOfLawTerm = function (areaOfLawTerm) {
 
                 cm.activeAOLTerm = areaOfLawTerm;
 
@@ -114,11 +130,69 @@
                 cm.activeSubAOLTerm = areaOfLawTerm.subareaTerms[0];
 
             }
-
-            cm.selectedSubAreaOfLawTerm = function (subAreaOfLawTerm) {
+            ////function to for seclection of subAOL items 
+            cm.selectSubAreaOfLawTerm = function (subAreaOfLawTerm) {
                 cm.activeSubAOLTerm = subAreaOfLawTerm;
+                
             }
 
+            cm.selectDocumentTemplateTypeLawTerm = function (documentTemplateTypeLawTerm) {
+                // alert(documentTemplateTypeLawTerm);
+                if (documentTemplateTypeLawTerm != null) {
+                    cm.removeDTItem = true;
+                    cm.activeDocumentTypeLawTerm = documentTemplateTypeLawTerm;
+                }
+
+            }
+
+            cm.addToDocumentTemplate = function () {
+                var   isThisNewDocTemplate = true;
+                if (cm.activeSubAOLTerm != null) {
+                    angular.forEach(cm.documentTypeLawTerms, function (term) { //For loop
+                        if (cm.activeSubAOLTerm.id == term.id) {// this line will check whether the data is existing or not
+
+                            isThisNewDocTemplate = false;
+                        }
+                    });
+                    if (isThisNewDocTemplate) {
+
+                        cm.documentTypeLawTerms.push(cm.activeSubAOLTerm);
+                        cm.activeSubAOLTerm = null;
+
+                    }
+                }
+            }
+
+            cm.removeFromDocumentTemplate = function () {
+                //  alert(cm.activeDocumentTypeLawTerm);
+                if (cm.removeDTItem) {
+                    var index = cm.documentTypeLawTerms.indexOf(cm.activeDocumentTypeLawTerm);
+                    cm.documentTypeLawTerms.splice(index, 1);
+                    cm.removeDTItem = false;
+                    cm.activeDocumentTypeLawTerm = null;
+                }
+               
+            }
+
+
+            cm.navigateToSecondSection = function (sectionName) {
+                if (sectionName == "snConflictCheck") {
+                    if (cm.clientId != null && cm.selectedClientName != null && (cm.matterName != null || cm.matterName.trim() != '') && (cm.matterId != null || cm.matterId.trim() != '') && (cm.matterDescription != null || cm.matterDescription.trim() != '')) {
+                        cm.sectionName = sectionName;
+
+                    }
+                    else {
+                        alert("Incomplete");
+                    }
+                }
+                else {
+                    cm.sectionName = sectionName;
+                }
+            }
+           
+            cm.saveDocumentTemplates = function () {
+
+            }
             //$scope.SelectModal= function(){
             //   // jQuery('#myModal').modal('show');
             //}
