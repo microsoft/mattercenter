358c6b200c6bb5028979b5ed496015b5cd30b6af
venkatesh127 <v-venmup@microsoft.com>
2016-05-01 22:18:30 -0700

    selectMatter type changes

--- tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Web/wwwroot/app/matter/createMatter.controller.js
+++ tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Web/wwwroot/app/matter/createMatter.controller.js
@@ -5,6 +5,7 @@
         .controller('createMatterController', ['$scope', '$state', '$stateParams', 'api',
         function ($scope, $state, $stateParams, api) {
             var cm = this;
+
             cm.clientNameList = [];
             cm.selectedClientName = null;
             cm.clientId = null;
@@ -13,8 +14,8 @@
             cm.areaOfLawTerms = [];
             cm.subAreaOfLawTerms = [];
             var optionsForClientGroup = new Object;
-            var optionsForPracticeGroup=new Object;
-            
+            var optionsForPracticeGroup = new Object;
+
             function getTaxonomyDetailsForClient(optionsForClientGroup, callback) {
                 api({
                     resource: 'matterResource',
@@ -37,7 +38,7 @@
 
             optionsForClientGroup = {
                 Client: {
-                   
+
                     Url: "https://msmatter.sharepoint.com/sites/catalog"
                 },
                 TermStoreDetails: {
@@ -49,7 +50,7 @@
 
             optionsForPracticeGroup = {
                 Client: {
-                   
+
                     Url: "https://msmatter.sharepoint.com/sites/catalog"
                 },
                 TermStoreDetails: {
@@ -58,49 +59,64 @@
                     CustomPropertyName: "ClientURL"
                 }
             }
-                
-            
+
+            //call back function for getting the clientNamesList
             getTaxonomyDetailsForClient(optionsForClientGroup, function (response) {
-                
-               
+
+
                 cm.clientNameList = response.clientTerms;
                 cm.popupContainerBackground = "hide";// jQuery('#myModal').modal('show');
-                //alert(cm.popupContainerBackground);
+
 
             });
-            
+
             cm.selectMatterType = function () {
                 cm.popupContainerBackground = "Show";
                 cm.popupContainer = "Show";
                 getTaxonomyDetailsForPractice(optionsForPracticeGroup, function (response) {
-                  
-                  //  console.log(response.pgTerms);
                     cm.pracitceGroupList = response.pgTerms;
-                    
-
                 });
-                
+            }
+
+
+            cm.selectMatterTypePopUpClose = function () {
+                alert();
+                cm.popupContainerBackground = "hide";
+                cm.popupContainer = "hide";
             }
 
             cm.getSelectedClientValue = function (client) {
-              //  console.log(cm.selectedClientName);
+
                 cm.clientId = cm.selectedClientName;
-                //alert(client.name+","+client.id);
+
             }
             cm.getSelectedPracticeGroupValue = function () {
 
-                console.log(cm.selectedPracticeGroup);
-                cm.areaOfLawTerms = cm.selectedPracticeGroup.areaTerms;
-               // var aolContent = " <div class='popUpMDContent ' ng-repeat='areaTerm in cm.selectedPracticeGroup.areaTerms' data-value='0' documents='' data-foldernamesaol='Emails;' data-areaoflaw-id=''></div>";
-               
+
+                if (cm.selectedPracticeGroup != null) {
+                    cm.areaOfLawTerms = cm.selectedPracticeGroup.areaTerms;
+                    cm.subAreaOfLawTerms = cm.selectedPracticeGroup.areaTerms[0].subareaTerms;
+                    cm.activeSubAOLTerm = cm.selectedPracticeGroup.areaTerms[0].subareaTerms[0];
+                    cm.activeAOLTerm = cm.selectedPracticeGroup.areaTerms[0];
+                } else {
+                    cm.areaOfLawTerms = cm.subAreaOfLawTerms = null;
+
+                }
+
             }
 
 
             cm.selectedAreaOfLawTerm = function (areaOfLawTerm) {
-                cm.aolSelectedClass = "popUpSelected";
-                // alert(areaOfLawTerm.termName);
-                cm.subAreaOfLawTerms = areaOfLawTerm.subareaTerms;
 
+                cm.activeAOLTerm = areaOfLawTerm;
+
+                cm.subAreaOfLawTerms = areaOfLawTerm.subareaTerms;
+                cm.activeSubAOLTerm = areaOfLawTerm.subareaTerms[0];
+
+            }
+
+            cm.selectedSubAreaOfLawTerm = function (subAreaOfLawTerm) {
+                cm.activeSubAOLTerm = subAreaOfLawTerm;
             }
 
             //$scope.SelectModal= function(){
