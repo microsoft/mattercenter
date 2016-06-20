c14775474fa2aef279fe126b9296b47b1593a7c7
venkatesh127 <v-venmup@microsoft.com>
2016-06-20 07:02:09 -0700

    Merged the design issue for grid in 320px width

--- tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Web/wwwroot/app/matter/matters.controller.js
+++ tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Web/wwwroot/app/matter/matters.controller.js
@@ -1365,46 +1365,46 @@
 
             //#region setting the grid options when window is resized
 
-            //angular.element($window).bind('resize', function () {
-            //    if ($window.innerWidth < 340) {
-            //        vm.gridOptions.enableGridMenu = false;
-            //        vm.gridOptions.enablePaginationControls = false;
-            //        vm.gridOptions.columnDefs = [{ field: 'matterName', displayName: 'Matter', enableHiding: false, width: "100%", cellTemplate: '../app/matter/MatterTemplates/MatterCellTemplate.html', headerCellTemplate: '../app/matter/MatterTemplates/MatterHeaderTemplate.html' }];
-            //        $scope.$apply();
-            //    } else {
-            //        vm.gridOptions = {
-            //            paginationPageSizes: [10, 50, 100],
-            //            paginationPageSize: 10,
-            //            enableGridMenu: true,
-            //            enableRowHeaderSelection: false,
-            //            enableRowSelection: true,
-            //            enableSelectAll: false,
-            //            multiSelect: false,
-            //            enablePaginationControls: true,
-            //            columnDefs: [
-            //                 { field: 'matterName', displayName: 'Matter', enableHiding: false, width: "20%", cellTemplate: '../app/matter/MatterTemplates/MatterCellTemplate.html', headerCellTemplate: '../app/matter/MatterTemplates/MatterHeaderTemplate.html' },
-            //                 { field: 'matterClient', displayName: 'Client', enableCellEdit: true, width: "15%", headerCellTemplate: '../app/matter/MatterTemplates/ClientHeaderTemplate.html' },
-            //                 { field: 'matterClientId', displayName: 'Client.MatterID', width: "15%", headerTooltip: 'Click to sort by client.matterid', cellTemplate: '<div class="ui-grid-cell-contents" >{{row.entity.matterClientId}}.{{row.entity.matterID}}</div>', enableCellEdit: true, },
-            //                 { field: 'matterModifiedDate', displayName: 'Modified Date', width: "10%", cellTemplate: '<div class="ui-grid-cell-contents"  datefilter date="{{row.entity.matterModifiedDate}}"></div>', headerCellTemplate: '../app/matter/MatterTemplates/ModifiedDateTemplate.html' },
-            //                 { field: 'matterResponsibleAttorney', headerTooltip: 'Click to sort by attorney', width: "15%", displayName: 'Responsible attorney', visible: false },
-            //                 { field: 'matterSubAreaOfLaw', headerTooltip: 'Click to sort by sub area of law', width: "15%", displayName: 'Sub area of law', visible: false },
-            //                 { field: 'matterCreatedDate', headerTooltip: 'Click to sort by matter open date', width: "15%", displayName: 'Open date', cellTemplate: '<div class="ui-grid-cell-contents" datefilter date="{{row.entity.matterCreatedDate}}"></div>', visible: false },
-            //            ],
-            //            enableColumnMenus: false,
-            //            onRegisterApi: function (gridApi) {
-            //                $scope.gridApi = gridApi;
-            //                gridApi.core.on.columnVisibilityChanged($scope, function (changedColumn) {
-            //                    $scope.columnChanged = { name: changedColumn.colDef.name, visible: changedColumn.colDef.visible };
-            //                });
-            //                gridApi.selection.on.rowSelectionChanged($scope, function (row) {
-            //                    vm.selectedRow = row.entity
-            //                });
-            //                $scope.gridApi.core.on.sortChanged($scope, $scope.sortChanged);
-            //                $scope.sortChanged($scope.gridApi.grid, [vm.gridOptions.columnDefs[1]]);
-            //            }
-            //        };
-            //    }
-            //});
+            angular.element($window).bind('resize', function () {
+                if ($window.innerWidth < 360) {
+                    vm.gridOptions.enableHorizontalScrollbar = false;
+                    vm.gridOptions.enablePaginationControls = false;
+                    vm.gridOptions.columnDefs = [{ field: 'matterName', displayName: 'Matter', enableHiding: false, width: "100%", cellTemplate: '../app/matter/MatterTemplates/MatterCellTemplate.html', headerCellTemplate: '../app/matter/MatterTemplates/MatterHeaderTemplate.html' }];
+                    $scope.$apply();
+                } else {                   
+                    vm.gridOptions = {
+                        enableHorizontalScrollbar: 0,
+                        enableVerticalScrollbar: 1,
+                        enableGridMenu: true,
+                        enableRowHeaderSelection: false,
+                        enableRowSelection: true,
+                        enableSelectAll: false,
+                        multiSelect: false,
+                        columnDefs: [
+                             { field: 'matterName', displayName: 'Matter', enableHiding: false, width: "245", cellTemplate: '../app/matter/MatterTemplates/MatterCellTemplate.html', headerCellTemplate: '../app/matter/MatterTemplates/MatterHeaderTemplate.html' },
+                             { field: 'matterClient', displayName: 'Client', enableCellEdit: true, width: "200", headerCellTemplate: '../app/matter/MatterTemplates/ClientHeaderTemplate.html' },
+                             { field: 'matterClientId', displayName: 'Client.MatterID', width: "150", headerCellTemplate: $templateCache.get('coldefheadertemplate.html'), cellTemplate: '<div class="ui-grid-cell-contents" >{{row.entity.matterClientId}}.{{row.entity.matterID}}</div>', enableCellEdit: true, },
+                             { field: 'matterModifiedDate', displayName: 'Modified Date', width: "195", cellTemplate: '<div class="ui-grid-cell-contents"  datefilter date="{{row.entity.matterModifiedDate}}"></div>', headerCellTemplate: '../app/matter/MatterTemplates/ModifiedDateTemplate.html' },
+                             { field: 'matterResponsibleAttorney', headerCellTemplate: '../app/matter/MatterTemplates/ResponsibleAttorneyHeaderTemplate.html', width: "250", displayName: 'Responsible attorney', visible: false },
+                             { field: 'matterSubAreaOfLaw', headerCellTemplate: '../app/matter/MatterTemplates/AreaofLawHeaderTemplate.html', width: "210", displayName: 'Sub area of law', visible: false },
+                             { field: 'matterCreatedDate', headerCellTemplate: '../app/matter/MatterTemplates/OpenDateTemplate.html', width: "170", displayName: 'Open date', cellTemplate: '<div class="ui-grid-cell-contents" datefilter date="{{row.entity.matterCreatedDate}}"></div>', visible: false },
+                        ],
+                        enableColumnMenus: false,
+                        onRegisterApi: function (gridApi) {
+                            $scope.gridApi = gridApi;
+                            gridApi.core.on.columnVisibilityChanged($scope, function (changedColumn) {
+                                $scope.columnChanged = { name: changedColumn.colDef.name, visible: changedColumn.colDef.visible };
+                            });
+                            gridApi.selection.on.rowSelectionChanged($scope, function (row) {
+                                vm.selectedRow = row.entity
+                            });
+                            $scope.gridApi.core.on.sortChanged($scope, $scope.sortChanged);
+                            $scope.sortChanged($scope.gridApi.grid, [vm.gridOptions.columnDefs[1]]);
+                        }
+                    };
+                    $scope.$apply();
+                }
+            });
 
             //#endregion
 
