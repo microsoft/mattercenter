ebb80306f5eb32ee2c13cb6685703eceb2fad0c7
venkatesh127 <v-venmup@microsoft.com>
2016-06-24 06:14:58 -0700

    Fixed design and responsive issues in matters and Added the infinite scroll functionality in matters.

--- tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Web/wwwroot/app/config.js
+++ tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Web/wwwroot/app/config.js
@@ -1,23 +1,25 @@
 var configs = {
-    "uri":{
-         "SPOsiteURL": "https://msmatter.sharepoint.com",
-         "tenant": "msmatter.onmicrosoft.com",
-     }, 
-    "ADAL" : { 
+    "uri": {
+        "SPOsiteURL": "https://msmatter.sharepoint.com",
+        "tenant": "msmatter.onmicrosoft.com",
+    },
+    "ADAL": {
         "clientId": "b94f07df-c825-431f-b9c5-b9499e8e9ac1"
     },
-    "uploadMessages":{
-        "maxAttachedMessage" :"Do not select more than five documents to attach at one time.",
-        "attachSuccessMessage":"Documents successfully attached.",
-        "attachInProgressMessage":"",
-        "attachFailureMessage":"One or more of your selected documents failed to attach:",
-        "attachButtonText":"Attach Documents",
-       "overwrite_Config_Property":"Email Only",
-       "upload_Append_Button":"Append date to file name and save",
-       "upload_Append_Button_Tooltip":"The file will be saved as new, separate document with the current date and time added to the end of the file name.",
-       "content_Check_Abort": "Content check has been aborted."
+    "uploadMessages": {
+        "maxAttachedMessage": "Do not select more than five documents to attach at one time.",
+        "attachSuccessMessage": "Documents successfully attached.",
+        "attachInProgressMessage": "",
+        "attachFailureMessage": "One or more of your selected documents failed to attach:",
+        "attachButtonText": "Attach Documents",
+        "overwrite_Config_Property": "Email Only",
+        "upload_Append_Button": "Append date to file name and save",
+        "upload_Append_Button_Tooltip": "The file will be saved as new, separate document with the current date and time added to the end of the file name.",
+        "content_Check_Abort": "Content check has been aborted.",
+        "uploadImageDocumentIcon": "/_layouts/15/images/ic{0}.gif",
+        "uploadPNGIconExtensions": "pdf"
     },
-    "global":{
-        repositoryUrl:"https://msmatter.sharepoint.com/sites/catalog",        
+    "global": {
+        repositoryUrl: "https://msmatter.sharepoint.com/sites/catalog",
     }
- };
+};
