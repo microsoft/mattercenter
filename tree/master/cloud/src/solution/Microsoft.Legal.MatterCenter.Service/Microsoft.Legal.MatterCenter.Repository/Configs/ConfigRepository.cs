8c38ffb3117ca7ad76ece637a8590fc21efd6d08
Aisling Duggan <aislid@microsoft.com>
2016-08-03 12:44:33 -0700

    Mattercenter2 (#307)
    * Addin InsertAndUpdate for configs.
    
    * Insert Update config API
    
    * Final changes  for Insert/Update
    
    * Changes for RC2 to RTM upgrade
    
    * Changed to make the Table and Column names for UIConfiguration configurable and added a helper method for getting the Cloud table reference

--- tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Service/Microsoft.Legal.MatterCenter.Repository/Configs/ConfigRepository.cs
+++ tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Service/Microsoft.Legal.MatterCenter.Repository/Configs/ConfigRepository.cs
@@ -9,7 +9,8 @@ using System.Collections.Generic;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json;
+using System.Globalization;
 
 namespace Microsoft.Legal.MatterCenter.Repository
 {
@@ -17,16 +18,16 @@ namespace Microsoft.Legal.MatterCenter.Repository
     {
 
         private GeneralSettings generalSettings;
-        private LogTables logTables;
+        private UIConfigSettings configSettings;
         private IConfigRepository config;
 
 
         public ConfigRepository(
             IOptions<GeneralSettings> generalSettings,
-            IOptions<LogTables> logTables)
+              IOptions<UIConfigSettings> configSettings)
         {
             this.generalSettings = generalSettings.Value;
-            this.logTables = logTables.Value;
+            this.configSettings = configSettings.Value;
         }
 
 
@@ -40,6 +41,15 @@ namespace Microsoft.Legal.MatterCenter.Repository
         {
             return await Task.FromResult(this.GetConfigEntities(filter));
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="configRequest"></param>
+        /// <returns></returns>
+        public async Task<bool> InsertUpdateConfigurationsAsync(String configs)
+        {
+            return await Task.FromResult(UpdateEntityProperty(configs));
+        }
 
         /// <summary>
         /// Get all the current configs from the Config Storage table
@@ -51,40 +61,129 @@ namespace Microsoft.Legal.MatterCenter.Repository
             var entities = new List<DynamicTableEntity>();
             try
             {
-                CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(generalSettings.CloudStorageConnectionString);
-                CloudTableClient tableClient = cloudStorageAccount.CreateCloudTableClient();
-                tableClient.DefaultRequestOptions = new TableRequestOptions
-                {
-                    PayloadFormat = TablePayloadFormat.JsonNoMetadata
-                };
-                // Retrieve a reference to the table.
-                CloudTable table = tableClient.GetTableReference("MatterCenterConfiguration");
+
+                CloudTable table = GetTable();
                 TableQuery<DynamicTableEntity> query = new TableQuery<DynamicTableEntity>();
 
                 if (filter == "")
                 {
-
-                    // Construct the query operation for all  entities 
-                   query = new TableQuery<DynamicTableEntity>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "MatterCenterConfig"));
-
+                    // Construct the queryConfigGroup operation for all  entities 
+                    query = new TableQuery<DynamicTableEntity>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, configSettings.Partitionkey));
                 }
                 else
                 {
-                    query = new TableQuery<DynamicTableEntity>().Where(TableQuery.GenerateFilterCondition("ConfigGroup", QueryComparisons.Equal, filter));
+                    query = new TableQuery<DynamicTableEntity>().Where(TableQuery.GenerateFilterCondition(configSettings.ConfigGroup, QueryComparisons.Equal, filter));
                 }
 
                 var queryResult = table.ExecuteQuery(query);
-                
+
                 entities.AddRange(queryResult);
-                
+
             }
             catch (Exception)
             {
                 throw;
             }
+
             return entities;
         }
 
-    }
-}
 
+        /// <summary>
+        /// Update  or Insert Configuration values 
+        /// </summary>
+        /// <param name="configs"></param>
+        public bool UpdateEntityProperty(string configs)
+        {
+            try
+            {
+                // Retrieve a reference to the table.
+                CloudTable table = GetTable();
+          
+                TableBatchOperation batchOperation = new TableBatchOperation();
+
+                Dictionary<string, Dictionary<string, string>> allValues = new Dictionary<string, Dictionary<string, string>>();
+                Dictionary<string,string> keyValues = new Dictionary<string, string>();
+
+                Dictionary<string, EntityProperty> newProperties = new Dictionary<string, EntityProperty>();
+
+                //add all the configsGroups and their key value pairs to a dictionary
+                allValues = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(configs);
+
+                var batch = new TableBatchOperation();
+                TableQuery<DynamicTableEntity> queryFinal = new TableQuery<DynamicTableEntity>();
+       
+                foreach (KeyValuePair<string, Dictionary<string, string>> entry in allValues)
+                {                  
+                    foreach (KeyValuePair<string, string> keyValue in entry.Value)
+                    {                 
+                        TableQuery<DynamicTableEntity> entityQuery = new TableQuery<DynamicTableEntity>().Where(
+                            TableQuery.CombineFilters(
+                                TableQuery.GenerateFilterCondition(configSettings.ConfigGroup, QueryComparisons.Equal.ToLower(), entry.Key),
+                                TableOperators.And,
+                                TableQuery.GenerateFilterCondition(configSettings.Key, QueryComparisons.Equal, keyValue.Key)));
+
+                       IEnumerable<DynamicTableEntity> queryResult = table.ExecuteQuery(entityQuery);
+                       
+                        if (queryResult.GetEnumerator().MoveNext())
+                        {
+                            foreach (DynamicTableEntity entity in queryResult)                         
+                            {
+                                entity.Properties[configSettings.ConfigGroup].StringValue = entry.Key;
+                                entity.Properties[configSettings.Key].StringValue = keyValue.Key;
+                                entity.Properties[configSettings.Value].StringValue = keyValue.Value;
+                                batchOperation.Merge(entity);    
+                            }
+                        }
+                        else
+                        {
+                            DynamicTableEntity config = CreateEntity(entry, keyValue);
+                            batchOperation.InsertOrReplace(config);
+                        }                    
+                       
+                    }
+                }
+
+             table.ExecuteBatch(batchOperation);
+             return true;
+                         
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+     
+
+        }
+
+        private DynamicTableEntity CreateEntity(KeyValuePair<string, Dictionary<string, string>> entry, KeyValuePair<string, string> keyValue)
+        {
+            DynamicTableEntity config = new DynamicTableEntity();
+            config.PartitionKey = configSettings.Partitionkey;
+            config.RowKey = string.Format(CultureInfo.InvariantCulture, Guid.NewGuid().ToString());
+            config.Properties.Add(configSettings.ConfigGroup, new EntityProperty(entry.Key));
+            config.Properties.Add(configSettings.Key, new EntityProperty(keyValue.Key));
+            config.Properties.Add(configSettings.Value, new EntityProperty(keyValue.Value));
+
+            return config;
+        }
+
+        private CloudTable GetTable()
+        {
+
+            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(generalSettings.CloudStorageConnectionString);
+            CloudTableClient tableClient = cloudStorageAccount.CreateCloudTableClient();
+            tableClient.DefaultRequestOptions = new TableRequestOptions
+            {
+                PayloadFormat = TablePayloadFormat.JsonNoMetadata
+            };
+            // Retrieve a reference to the table.
+            CloudTable table = tableClient.GetTableReference(configSettings.MatterCenterConfiguration);
+
+            return table;
+        }
+
+    }
+
+   
+}
--- tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Service/Microsoft.Legal.MatterCenter.ServiceTest/IntegrationTests/ConfigUnitTests.cs
+++ tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Service/Microsoft.Legal.MatterCenter.ServiceTest/IntegrationTests/ConfigUnitTests.cs
@@ -18,7 +18,7 @@ namespace Microsoft.Legal.MatterCenter.ServiceTest
     public class ConfigUnitTest
     {
 
-     
+
         private readonly TestServer testServer;
         private const string authority = "https://login.windows.net/microsoft.onmicrosoft.com";
 
@@ -28,7 +28,7 @@ namespace Microsoft.Legal.MatterCenter.ServiceTest
 
         public ConfigUnitTest()
         {
-        
+
             testServer = new TestServer(new WebHostBuilder().UseStartup<Startup>());
         }
 
@@ -47,21 +47,29 @@ namespace Microsoft.Legal.MatterCenter.ServiceTest
             var generalSettingsMoq = new Moq.Mock<IOptions<GeneralSettings>>();
             generalSettingsMoq.SetupGet(p => p.Value.CloudStorageConnectionString).Returns("DefaultEndpointsProtocol=https;AccountName=mattercenterlogstoragev0;AccountKey=Y3s1Wz+u2JQ/wl5WSVB5f+31oXyBlcdFVLk99Pgo8y8/vxSO7P8wOjbbWdcS7mAZLkqv8njHROc1bQj8d/QePQ==");
 
-            var environmentMoq = new Moq.Mock<IHostingEnvironment>(); 
+            var environmentMoq = new Moq.Mock<IHostingEnvironment>();
             environmentMoq.SetupGet(p => p.WebRootPath).Returns(@"C:\Repos\mcfork\tree\master\cloud\\src\solution\Microsoft.Legal.MatterCenter.Web\wwwroot");
 
             var matterCenterServiceFunctionsMoq = new Moq.Mock<IMatterCenterServiceFunctions>();
                
-            var logTablesMoq = new Moq.Mock<IOptions<LogTables>>();
+            var uiConfigsMoq = new Moq.Mock<IOptions<UIConfigSettings>>();
+            uiConfigsMoq.SetupGet(t => t.Value.MatterCenterConfiguration).Returns("MatterCenterConfiguration");
+            uiConfigsMoq.SetupGet(p => p.Value.Partitionkey).Returns("MatterCenterConfig");
+            uiConfigsMoq.SetupGet(c => c.Value.ConfigGroup).Returns("ConfigGroup");
+            uiConfigsMoq.SetupGet(k => k.Value.Key).Returns("Key");
+            uiConfigsMoq.SetupGet(v => v.Value.Value).Returns("Value");
 
-            ConfigRepository configRepository = new ConfigRepository(generalSettingsMoq.Object, logTablesMoq.Object);
+
+            var logTableMoq = new Moq.Mock<IOptions<LogTables>>();
+
+            ConfigRepository configRepository = new ConfigRepository(generalSettingsMoq.Object, uiConfigsMoq.Object);
 
             generalSettingsMoq.SetupGet(g => g.Value).Returns(genS);
             errorSettingsMoq.SetupAllProperties();
 
-        
-            ConfigController controller = new ConfigController( errorSettingsMoq.Object, generalSettingsMoq.Object, matterCenterServiceFunctionsMoq.Object, configRepository, environmentMoq.Object);
-     
+
+            ConfigController controller = new ConfigController(errorSettingsMoq.Object, generalSettingsMoq.Object, uiConfigsMoq.Object, logTableMoq.Object, matterCenterServiceFunctionsMoq.Object, configRepository, environmentMoq.Object);
+
             var result = controller.Get("");
 
             Assert.True(result.Status > 0);
@@ -85,33 +93,97 @@ namespace Microsoft.Legal.MatterCenter.ServiceTest
 
             var matterCenterServiceFunctionsMoq = new Moq.Mock<IMatterCenterServiceFunctions>();
 
-            var logTablesMoq = new Moq.Mock<IOptions<LogTables>>();
+            var uiConfigsMoq = new Moq.Mock<IOptions<UIConfigSettings>>();
+            uiConfigsMoq.SetupGet(t => t.Value.MatterCenterConfiguration).Returns("MatterCenterConfiguration");
+            uiConfigsMoq.SetupGet(p => p.Value.Partitionkey).Returns("MatterCenterConfig");
+            uiConfigsMoq.SetupGet(c => c.Value.ConfigGroup).Returns("ConfigGroup");
+            uiConfigsMoq.SetupGet(k => k.Value.Key).Returns("Key");
+            uiConfigsMoq.SetupGet(v => v.Value.Value).Returns("Value");
 
-            ConfigRepository configRepository = new ConfigRepository(generalSettingsMoq.Object, logTablesMoq.Object);
+            var logTableMoq = new Moq.Mock<IOptions<LogTables>>();
+
+            ConfigRepository configRepository = new ConfigRepository(generalSettingsMoq.Object, uiConfigsMoq.Object);
 
             generalSettingsMoq.SetupGet(g => g.Value).Returns(genS);
             errorSettingsMoq.SetupAllProperties();
 
-            ConfigController controller = new ConfigController(errorSettingsMoq.Object, generalSettingsMoq.Object, matterCenterServiceFunctionsMoq.Object, configRepository, environmentMoq.Object);
+            ConfigController controller = new ConfigController(errorSettingsMoq.Object, generalSettingsMoq.Object, uiConfigsMoq.Object, logTableMoq.Object, matterCenterServiceFunctionsMoq.Object, configRepository, environmentMoq.Object);
 
             var result = controller.Get("Home");
 
             Assert.True(result.Status > 0);
         }
 
+        [Fact]
+        public void InsertUpdateUIConfigs()
+        {
+
+            GeneralSettings genS = new GeneralSettings();
+            genS.CloudStorageConnectionString = "DefaultEndpointsProtocol = https; AccountName = mattercenterlogstoragev0; AccountKey = Y3s1Wz + u2JQ / wl5WSVB5f + 31oXyBlcdFVLk99Pgo8y8 / vxSO7P8wOjbbWdcS7mAZLkqv8njHROc1bQj8d / QePQ == ";
+
+            //Need to Mock the injected services and setup any properties on these that the test requires
+            var errorSettingsMoq = new Moq.Mock<IOptions<ErrorSettings>>();
+
+            var generalSettingsMoq = new Moq.Mock<IOptions<GeneralSettings>>();
+            generalSettingsMoq.SetupGet(p => p.Value.CloudStorageConnectionString).Returns("DefaultEndpointsProtocol=https;AccountName=mattercenterlogstoragev0;AccountKey=Y3s1Wz+u2JQ/wl5WSVB5f+31oXyBlcdFVLk99Pgo8y8/vxSO7P8wOjbbWdcS7mAZLkqv8njHROc1bQj8d/QePQ==");
+
+            var environmentMoq = new Moq.Mock<IHostingEnvironment>();
+            environmentMoq.SetupGet(p => p.WebRootPath).Returns(@"C:\Repos\mcfork\tree\master\cloud\\src\solution\Microsoft.Legal.MatterCenter.Web\wwwroot");
+
+            var matterCenterServiceFunctionsMoq = new Moq.Mock<IMatterCenterServiceFunctions>();
+
+            var uiConfigsMoq = new Moq.Mock<IOptions<UIConfigSettings>>();
+            uiConfigsMoq.SetupGet(t => t.Value.MatterCenterConfiguration).Returns("MatterCenterConfiguration");
+            uiConfigsMoq.SetupGet(p => p.Value.Partitionkey).Returns("MatterCenterConfig");
+            uiConfigsMoq.SetupGet(c => c.Value.ConfigGroup).Returns("ConfigGroup");
+            uiConfigsMoq.SetupGet(k => k.Value.Key).Returns("Key");
+            uiConfigsMoq.SetupGet(v => v.Value.Value).Returns("Value");
+
+            var logTableMoq = new Moq.Mock<IOptions<LogTables>>();
+
+            ConfigRepository configRepository = new ConfigRepository(generalSettingsMoq.Object, uiConfigsMoq.Object);
+
+            generalSettingsMoq.SetupGet(g => g.Value).Returns(genS);
+            errorSettingsMoq.SetupAllProperties();
+
+            ConfigController controller = new ConfigController(errorSettingsMoq.Object, generalSettingsMoq.Object, uiConfigsMoq.Object, logTableMoq.Object, matterCenterServiceFunctionsMoq.Object, configRepository, environmentMoq.Object);
+
+
+            var configsStr = (@"{""Home"": {
+    ""ContextualHelpHeader"": ""Matter Center For Outlook"",
+    ""ContextualHelpBottomText"": ""Questions? Contact "",
+    ""HelpRequesURL"": ""https://msmatter.sharepoint.com/sites/catalog"",
+    ""MatterCenterSupportLinkText"": ""Matter Center Aisling Support"",
+    ""MatterCenterSupportLink"": ""mailto:support@supportsite.com"",
+    ""Change"": ""mailto:aisling@supportsite.com"",
+  },
+  ""MatterUsers"": {
+    ""SearchUsersURL"": ""https://msmatter.sharepoint.com/sites/client"",
+    ""StampedPropertiesURL"": ""https://msmatter.sharepoint.com/sites/microsoft"",
+    ""Add"": ""test"",
+  }}");
+
+
+
+            var result = controller.InsertUpdate(configsStr);
+
+            Assert.True(result.Status > 0);
+        }
+
+
 
 
         /// This unit test will try to get all the users who can see a particular item
         /// </summary>
         [Fact]
-        public  void GetConfigsFromTableRep()
+        public void GetConfigsFromTableRep()
         {
-            
+
 
             GeneralSettings genS = new GeneralSettings();
             genS.CloudStorageConnectionString  =  "DefaultEndpointsProtocol = https; AccountName = mattercenterlogstoragev0; AccountKey = Y3s1Wz + u2JQ / wl5WSVB5f + 31oXyBlcdFVLk99Pgo8y8 / vxSO7P8wOjbbWdcS7mAZLkqv8njHROc1bQj8d / QePQ == ";
             var m = new Moq.Mock<IOptions<GeneralSettings>>();
-            var l = new Moq.Mock<IOptions<LogTables>>();
+            var l = new Moq.Mock<IOptions<UIConfigSettings>>();
             
             m.SetupGet(g => g.Value).Returns(genS);
             m.SetupAllProperties();
@@ -125,3 +197,4 @@ namespace Microsoft.Legal.MatterCenter.ServiceTest
 
     }
 }
+
