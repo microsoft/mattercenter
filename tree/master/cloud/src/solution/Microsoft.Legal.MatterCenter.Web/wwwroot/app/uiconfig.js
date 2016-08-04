abbd7a8c7fc5ecda86424712f32215ad8aa6b8a8
kiran11990 <v-saigud@microsoft.com>
2016-08-04 23:47:23 +0530

    1. Added new css styles for the grid menu in site.css. (#313)
    2. Added changes for the grid menu functionality in matters and documents pages.
    3. Added new directive in common directives for the grid menu to work in responsive.
    4. Replaced hardcoded values with dynamic values in matters and navigation pages.

--- tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Web/wwwroot/app/uiconfig.js
+++ tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Web/wwwroot/app/uiconfig.js
@@ -1,5 +1,19 @@
 ﻿var uiconfigs =
 {
+  "Matters": {
+    "MatterType1": "All Matters",
+    "GridHeaderItem5": "Responsible attorney",
+    "GridHeaderItem6": "Sub area of law",
+    "GridHeaderItem7": "Open date",
+    "MatterType2": "My Matters",
+    "MatterType3": "Pinned Matters",
+    "SearchPlaceholderText": "Search by ID, or matter or client name",
+    "NoDataText": "Nothing matches your search term or the filter you selected. Please try again.<br>If you applied a filter, remove it to get back to your list.<br>Search suggestions:<br><ul><li>Make sure all words are spelled correctly</li><li>Try different keywords</li><li>Try searching by the matter name or client name</li><li>Try more general search terms</li></ul>",
+    "GridHeaderItem1": "Matter",
+    "GridHeaderItem2": "Client",
+    "GridHeaderItem3": "Client.MatterID",
+    "GridHeaderItem4": "Modified Date"
+  },
   "Home": {
     "ContextualHelpHeader": "Matter Center For Outlook",
     "ContextualHelpBottomText": "Questions? Contact ",
@@ -22,7 +36,13 @@
     "CreationContentTitle": "Create a new matter site",
     "HomeLinkContentTitle": "Go to the web home page",
     "LearnMoreText": "LearnMore",
-    "HomeMenuText": "Home"
+    "HomeMenuText": "Home",
+    "WelcomeText": "Welcome to Matter Center! Upload, share, and save your documents here.",
+    "Type1Title": "Search matters",
+    "Type2Title": "Search documents",
+    "ContactText": "Questions? Contact",
+    "MenuCaption": "menu",
+    "MenuCaptionTitle": "Matter Center Menu"
   },
   "MatterUsers": {
     "SearchUsersURL": "https://msmatter.sharepoint.com/sites/client",
