5d10630f8af179a4cfc259ccf6b97cc810bd6f3a
wilg <wilg@microsoft.com>
2016-08-19 15:21:43 -0700

    Matter center first version

---
+++ tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Web/wwwroot/app/config.js
@@ -0,0 +1,128 @@
+﻿var configs =
+{
+  "uri": {
+    "SPOsiteURL": "https://msmatter.sharepoint.com",
+    "tenant": "msmatter.onmicrosoft.com",
+    "MainURL": "http://www.microsoft.com/mattercenter"
+  },
+  "ADAL": {
+    "clientId": "b94f07df-c825-431f-b9c5-b9499e8e9ac1",
+    "authUserEmail": ""
+  },
+  "appInsights": {
+    "instrumentationKey": "0f913235-fa70-4fa4-ae7c-c753d2e0c35d",
+    "appType": ""
+  },
+  "global": {
+    "repositoryUrl": "https://msmatter.sharepoint.com/sites/catalog"
+  },
+  "matter": {
+    "StampedPropertyBlockedUploadUsers": "BlockedUploadUsers",
+    "StampedPropertyBlockedUsers": "BlockedUsers",
+    "StampedPropertyClientID": "ClientID",
+    "StampedPropertyClientName": "ClientName",
+    "StampedPropertyConflictCheckBy": "MatterConflictCheckBy",
+    "StampedPropertyConflictCheckDate": "MatterConflictCheckDate",
+    "StampedPropertyDefaultContentType": "MatterCenterDefaultContentType",
+    "StampedPropertyDocumentTemplateCount": "DocumentTemplateCount",
+    "StampedPropertyIsConflictIdentified": "IsConflictIdentified",
+    "StampedPropertyIsMatter": "IsMatter",
+    "StampedPropertyMatterCenterPermissions": "MatterCenterPermissions",
+    "StampedPropertyMatterCenterRoles": "MatterCenterRoles",
+    "StampedPropertyMatterCenterUserEmails": "MatterCenterUserEmails",
+    "StampedPropertyMatterCenterUsers": "MatterCenterUsers",
+    "StampedPropertyMatterDescription": "MatterDescription",
+    "StampedPropertyMatterGUID": "MatterGUID",
+    "StampedPropertyMatterID": "MatterID",
+    "StampedPropertyMatterName": "MatterName",
+    "StampedPropertyOpenDate": "OpenDate",
+    "StampedPropertyResponsibleAttorney": "ResponsibleAttorney",
+    "StampedPropertyResponsibleAttorneyEmail": "ResponsibleAttorneyEmail",
+    "StampedPropertySecureMatter": "SecureMatter",
+    "StampedPropertySuccess": "Success",
+    "StampedPropertyTeamMembers": "TeamMembers"
+  },
+  "taxonomy": {
+    "levels": "3"
+  },
+  "search": {
+    "ManagedPropertyAreaOfLaw": "MCAreaofLaw",
+    "ManagedPropertyAuthor": "MSITOfficeAuthor",
+    "ManagedPropertyBlockedUploadUsers": "MCBlockedUploadUser",
+    "ManagedPropertyCheckOutByUser": "CheckoutUserOWSUSER",
+    "ManagedPropertyClientID": "MCClientID",
+    "ManagedPropertyClientName": "MCClientName",
+    "ManagedPropertyCreated": "Created",
+    "ManagedPropertyCustomTitle": "RefinableString10",
+    "ManagedPropertyDescription": "Description",
+    "ManagedPropertyDocumentCheckOutUser": "MCCheckoutUser",
+    "ManagedPropertyDocumentClientId": "MCDocumentClientID",
+    "ManagedPropertyDocumentClientName": "MCDocumentClientName",
+    "ManagedPropertyDocumentId": "dlcDocIdOWSText",
+    "ManagedPropertyDocumentLastModifiedTime": "MCModifiedDate",
+    "ManagedPropertyDocumentMatterId": "RefinableString12",
+    "ManagedPropertyDocumentMatterName": "RefinableString13",
+    "ManagedPropertyDocumentVersion": "MCVersionNumber",
+    "ManagedPropertyFileExtension": "FileExtension",
+    "ManagedPropertyFileName": "FileName",
+    "ManagedPropertyIsDocument": "IsDocument",
+    "ManagedPropertyIsMatter": "MCIsMatter",
+    "ManagedPropertyLastModifiedTime": "LastModifiedTime",
+    "ManagedPropertyMatterGuid": "MatterCenterMatterGUID",
+    "ManagedPropertyMatterGuidLogging": "",
+    "ManagedPropertyMatterId": "MCMatterID",
+    "ManagedPropertyMatterName": "MCMatterName",
+    "ManagedPropertyName": "Name",
+    "ManagedPropertyOpenDate": "MCOpenDate",
+    "ManagedPropertyPath": "Path",
+    "ManagedPropertyPracticeGroup": "MCPracticeGroup",
+    "ManagedPropertyResponsibleAttorney": "MCResponsibleAttorney",
+    "ManagedPropertyServerRelativeUrl": "ServerRelativeURL",
+    "ManagedPropertySiteName": "SiteName",
+    "ManagedPropertySiteTitle": "SiteTitle",
+    "ManagedPropertySPWebUrl": "SPWebUrl",
+    "ManagedPropertySubAreaOfLaw": "MCSubAreaofLaw",
+    "ManagedPropertyTeamMembers": "MCTeamMembers",
+    "ManagedPropertyTitle": "Title",
+    "ManagedPropertyUIVersionStringOWSTEXT": "UIVersionStringOWSTEXT",
+    "searchColumnsUIPickerForMatter": {
+      "Column1": "matterName",
+      "Column2": "matterClient",
+      "Column3": "matterClientId",
+      "Column4": "matterModifiedDate",
+      "Column5": "matterResponsibleAttorney",
+      "Column6": "matterSubAreaOfLaw",
+      "Column7": "matterCreatedDate"
+    },
+    "searchColumnsUIPickerForDocument": {
+      "Column1": "documentName",
+      "Column2": "documentClient",
+      "Column3": "documentClientId",
+      "Column4": "documentModifiedDate",
+      "Column5": "documentOwner",
+      "Column6": "documentVersion",
+      "Column7": "documentCheckoutUser",
+      "Column8": "documentCreatedDate"
+    }
+  },
+  "contentTypes": {
+    "managedColumns": {
+      "ColumnName1": "PracticeGroup",
+      "ColumnName2": "AreaOfLaw",
+      "ColumnName3": "SubareaOfLaw"
+    }
+  },
+  "uploadMessages": {
+    "maxAttachedMessage": "Do not select more than five documents to attach at one time.",
+    "attachSuccessMessage": "Documents successfully attached.",
+    "attachFailureMessage": "One or more of your selected documents failed to attach:",
+    "attachButtonText": "Attach Documents",
+    "overwrite_Config_Property": "Email Only",
+    "upload_Append_Button": "Append date to file name and save",
+    "upload_Append_Button_Tooltip": "The file will be saved as new, separate document with the current date and time added to the end of the file name.",
+    "content_Check_Abort": "Content check has been aborted.",
+    "uploadImageDocumentIcon": "/_layouts/15/images/ic{0}.gif",
+    "uploadPNGIconExtensions": "pdf",
+    "attachInProgressMessage": ""
+  }
+}
