a13b1e12f5463f090389fcf3964953e7ffcf583a
premchandpl <v-lapedd@microsoft.com>
2016-09-29 13:02:17 -0700

    Code fixes for pin and unpin functionality (#443)
    * Modified Code for project center requirements so that code is compatible with both Matter Center and Project Center
    
    * Enabled backward comparability for folder structure setting.
    
    * merged code related to document sorting in pinned matters
    
    * Updated appsettings.json with correct values
    
    * Added code for matter flyout
    
    * Fixed the dashboard issue for project center and fixed the mail cart functionality for office docs.
    
    * Modified code to add a new property called "displayInDashboard" to config.js and which is used in dashboard pages
    
    * Code fixes related to Project center on matter center code base
    
    * Code fixes for Pin and Unpin functionality from the matter and document sharepoint pages
    
    * Code fix for pin and unpin functionality
    
    * Fixed the Unpin issue for matters
    Document UI issue has been addressed

--- tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Service/Microsoft.Legal.MatterCenter.Utility/ConfigSettings/GeneralSettings.cs
+++ tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Service/Microsoft.Legal.MatterCenter.Utility/ConfigSettings/GeneralSettings.cs
@@ -35,7 +35,7 @@ namespace Microsoft.Legal.MatterCenter.Utility
         public string MatterCenterConfiguration { get; set; }
         public string KeyVaultURI { get; set; }
         public string GraphUrl { get; set; }
-        public bool IsBackwardCompatibale { get; set; }
+        public bool IsBackwardCompatible { get; set; }
 
     }
 }
