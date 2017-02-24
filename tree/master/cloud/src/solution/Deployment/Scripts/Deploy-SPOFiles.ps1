ad57e75f34b3771fd18a1dd20664dccbd6767f6d
aislid <aislid@AISLINGDESK>
2017-02-23 18:26:31 -0800

    Undoing Local changes

--- tree/master/cloud/src/solution/Deployment/Scripts/Deploy-SPOFiles.ps1
+++ tree/master/cloud/src/solution/Deployment/Scripts/Deploy-SPOFiles.ps1
@@ -70,7 +70,9 @@ $ParentDirectory = (Get-ParentDirectory)
 # Include Common functions script
 #----------------------------------------------
 
-
+Show-Message -Message "Adding common library functions" -Type ([MessageType]::Warning)
+."$ScriptDirectory\LibraryFunctions.ps1"
+Show-Message -Message "Added common library functions" -Type ([MessageType]::Success)
 
 
 #Create Log folder if not exist
@@ -121,18 +123,75 @@ Function Deploy-SPOFiles
 	else {
 		Show-Message -Message "Completed uploading files to SharePoint library" -Type ([MessageType]::Success)
 	}
-   
+    
+    #---------------------------------------------------------------------
+    # Provisioning Web Dashboard page(s) on SharePoint library
+    #---------------------------------------------------------------------
+    Show-Message -Message "Provisioning Web dashboard"
+    & "$HelperPath\Microsoft.Legal.MatterCenter.ProvisionWebDashboard.exe" "true" $Username $Password $WebSiteName
+
+    If ((Get-Content $ErrorLogFile) -ne $Null) {
+		Show-Message -Message "Provisioning Web dashboard failed" -Type ([MessageType]::Failure)  
+    }
+    else {
+		Show-Message -Message "Completed Provisioning Web dashboard" -Type ([MessageType]::Success)
+    }
+
+	cd $PSScriptRoot
     
 }
-$SPCredential = Get-Credential -Message "Enter credentials to access SharePoint tenant."
-$SPPassword = $SPCredential.GetNetworkCredential().Password
-$WebAppName = 'MatterCenterWeb'
-
-	
 
 $UIUrl = [string]::format("https://{0}.azurewebsites.net", $WebAppName)
 Deploy-SPOFiles -WebSiteName $UIUrl  -UserName $SPCredential.UserName -PassWord $SPPassword 
 
 
+#----------------------------------------------
+# Update Office, Outlook and SharePoint App schema files
+#----------------------------------------------
+cd $HelperPath
+Show-Message -Message "Step : Update Office, Outlook and SharePoint App schema files"
+& "$HelperPath\Microsoft.Legal.MatterCenter.UpdateAppConfig.exe" "1" $SPCredential.UserName $SPPassword $UIUrl
 
+If ((Get-Content $ErrorLogFile) -ne $Null) {
+	Show-Message -Message "Updating Office, Outlook and SharePoint App schema files failed" -Type ([MessageType]::Failure)
+    return
+}
+else {
+	Show-Message -Message "Completed updating Office, Outlook and SharePoint App schema files" -Type ([MessageType]::Success)
+}
+
+cd $PSScriptRoot
+
+
+#----------------------------------------------
+# Add Apps to Office
+#----------------------------------------------
+Show-Message -Message "Step : Add and install apps to SharePoint and Office"
+."$ScriptDirectory\DeployOfficeApp.ps1" -IsDeploy: $true 
+
+
+    
+If ((Get-Content $ErrorLogFile) -ne $Null) {
+	Show-Message -Message "Adding and installing apps to  Office failed" -Type ([MessageType]::Failure)
+    return
+}
+else {
+	Show-Message -Message "Completed adding and installing apps to Office" -Type ([MessageType]::Success)
+}
+
+
+#----------------------------------------------
+# Add Apps to Exchange
+#----------------------------------------------
+$ExchangeCredential = Get-Credential -Message "Enter credentials to connect with Exchange server."
+Show-Message -Message "Step : Add apps to Exchange"
+. "$ScriptDirectory\DeployOutlookApp.ps1" -IsDeploy: $true
+    
+If ((Get-Content $ErrorLogFile) -ne $Null) {
+	Show-Message -Message "Adding apps to Exchange failed" -Type ([MessageType]::Failure)
+    return
+}
+else {
+	Show-Message -Message "Completed adding apps to Exchange" -Type ([MessageType]::Success)
+}
 
